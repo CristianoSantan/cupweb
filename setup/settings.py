39f761856b0cc66dfddaa3944bfe33e8cfabe30c
CristianoSantan <cristiano.mer@gmail.com>
2024-04-12 20:03:46 -0300

    bd postgre remoto

--- setup/settings.py
+++ setup/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 from decouple import config
+from dj_database_url import parse as db_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -71,12 +72,12 @@ WSGI_APPLICATION = "setup.wsgi.application"
 
 
 # Database
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": config(
+        "DATABASE_URL", default=f'sqlite://{BASE_DIR / "db.sqlite3"}', cast=db_url
+    )
 }
 
 
